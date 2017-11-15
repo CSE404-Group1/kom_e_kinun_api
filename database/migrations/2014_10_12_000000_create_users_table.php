c0f9a4d0ef8bad8255027a2726f0385ab20ad42b
IrtezaAsadRizvi <irteza.asad.cse@ulab.edu.bd>
2017-11-15 22:28:31 +0600

    wrote migration, next: figure relations and migrate

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            //need to add image
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->text('description')->nullable();
+            $table->string('phone_num');
+            $table->string('facebook_page');
+            $table->string('location');
+            $table->string('website')->nullable();
+            $table->boolean('is_superadmin');
+            $table->boolean('is_seller');
+            $table->boolean('is_premium');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2017_11_15_160147_create_items_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            //need to add image
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->float('actual_price');
+            $table->float('sale_price');
+            $table->dateTimeTz('offer_start_date');
+            $table->dateTimeTz('offer_end_date');
+            $table->integer('quantity')->nullable();
+            $table->string('offer_description')->nullable();
+            $table->integer('seller_id');
+            $table->string('brand_name');
+            $table->string('product_origin_page')->nullable();
+            $table->string('catagory');
+            $table->string('sub_catagory_1');
+            $table->string('sub_catagory_2');
+            $table->string('sub_catagory_3');
+            $table->text('keywords');
+            $table->boolean('is_featured');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
